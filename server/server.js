837685df6a14253dabbe8bd9115e87f5ec6df8c0
lemonboi666 <143658618+lemonboi666@users.noreply.github.com>
2024-01-08 17:22:51 -0500

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const dataRoutes = require('./routes/dataRoutes');
-const cors = require('cors');  // Add this line
+    const express = require('express');
+    const path = require('path');
+    const mongoose = require('mongoose');
+    const dataRoutes = require('./routes/dataRoutes');
+    const cors = require('cors');  // Add this line
+
+    mongoose.set('strictQuery', true);
 
 
-require('dotenv').config();
+    require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+    const app = express();
+    const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.json());
+    app.use(express.static(path.join(__dirname, '../public')));
+    app.use(express.json());
 
-app.use('/', dataRoutes);
+    app.use('/', dataRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
